608b7a525ed6758ca5ac200fc893b0d366bb04a9
Bemon <tristan2107b@gmail.com>
2023-06-26 18:41:17 +1000

    Added active time and date, bug fixes and assets folder

--- assets/script.js
+++ assets/script.js
@@ -61,6 +61,14 @@ $(function () {
     $(this).children('.description').val(value);
   });
 
+  function updateTime() {
+    const dateElement = $('#date');
+    const timeElement = $('#time');
+    const currentDate = dayjs().format('dddd, MMMM D, YYYY');
+    const currentTime = dayjs().format('hh:mm:ss A');
+    dateElement.text(currentDate);
+    timeElement.text(currentTime);
+  }
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   (function() {
@@ -69,4 +77,5 @@ $(function () {
     refreshColor();
   })();
   // TODO: Add code to display the current date in the header of the page.
+  setInterval(updateTime, 1000);
 });
